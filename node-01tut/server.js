51556bbfae750c8c0823d1068c9c96883b784310
Abdullah_Zayed <abdullah@quadratechsoft.com>
2023-01-12 09:54:24 +0200

    upload node js tutorial directory

---
+++ node-01tut/server.js
@@ -0,0 +1,9 @@
+const os = require("os");
+const path = require("os");
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
